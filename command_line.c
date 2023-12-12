a1a57464042f21db8f077e64267a38fad0bd9bdd
Heba <hebamagde@gmail.com>
2023-12-13 01:28:21 +0200

    update

--- command_line.c
+++ command_line.c
@@ -1,76 +1,58 @@
-#include "simple_shell.h"
+#include "new_shell.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
 /**
- * *interpreter - function that reads user input
- * Return: cmd
+ * *input - function that simulates command line
+ * @cmd: pointer to buffer of the entered comand
+ * @n: size of the buffer
+ * @user_input: number of characters resulted from getline
+ * Return: args
 */
 
-char *interpreter()
+char *input(char *cmd, size_t n, ssize_t user_input)
 {
-char *cmd = NULL;
-size_t n = 0;
-ssize_t input;
 
-    if (isatty(STDIN_FILENO)) 
-       write(STDOUT_FILENO, "$ ",3);
-    input = getline(&cmd, &n, stdin);
-    if (input == -1)
+char *cmd_cpy = NULL;
+char *delim = " \n";
+char *token = NULL, **args = NULL;
+int len, i = 0;
+    
+len = _strlen(cmd);
+cmd_cpy = malloc(sizeof(char) * user_input);
+    
+    if (cmd_cpy == NULL)
     {
-        write(STDOUT_FILENO, "\n", 1);
-        free(cmd);
-        exit(1);
+        perror("Error");
+        return (NULL);
     }
-    remove_newline(cmd);
-    if (_strcmp(cmd, "exit") == 0) 
-    {
-        exit(1);
-    }
+_strncpy(cmd_cpy, cmd, len - 1);
 
-
-return(cmd);
-}
-
-/**
- * execution - function that parse and excute commands
- * @cmd: command to be parsed and executed
- * Return: void
-*/
-
-void execution(char *cmd)
-{
-    pid_t pid;
-    char **args = NULL;
-if (access(cmd, X_OK) == 0) {
-         pid = fork();
-         if (pid == -1) 
-         {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        if (pid == 0) 
-        {
-            args = malloc(2 * sizeof(char));
-            if (args == NULL) {
-                perror("malloc");
-                exit(EXIT_FAILURE);
+            token = strtok(cmd, delim);
+            while(token)
+            {
+                token = strtok(NULL, delim);
+                i++;
             }
-            args[0] = cmd;
-            args[1] = NULL;
-            execve(cmd, args, NULL);           
-            perror("execve");
-            free(args);
-            exit(EXIT_FAILURE);
-        } else {            
-            wait(NULL); 
-        }
-    } else {
-        write(STDOUT_FILENO, "./shell: Command not found\n", 27);
-    }
+            args = malloc(sizeof(char) * (i + 1));
+            token = strtok(cmd_cpy, delim);
+	    if (args == NULL)
+	    {
+		    perror("Error");
+		    return (NULL);
+	    }
+	    token = strtok(cmd_cpy, delim);
+            i = 0;
+            while(token)
+            {
+                args[i] = token;
+                token = strtok(NULL, delim);
+                i++;
+            }
+            args[i] = NULL;
+    free (cmd_cpy);
+    
+    return (*args);
 }
 
