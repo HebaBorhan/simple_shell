f4f2d14b52a0ff877a22cfcec5fa0ebbed849db2
HebaZaki <h_borhan@hotmail.com>
2023-12-13 18:09:00 +0200

    header file

--- new_shell.h
+++ new_shell.h
@@ -7,10 +7,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <sys/stat.h>
 
 int _strlen(const char *string);
 void remove_line(char *command, char **lineremoved);
 char *_strncpy(char *dest, const char *src, int n);
 char *input(char *cmd, size_t n, ssize_t user_input);
+
+/**
+ * char *search_path(char **args);
+ */
 
 #endif
