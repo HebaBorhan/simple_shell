3075a9a33a392c9ada4ebbcec2bac0cfb4163d55
HebaZaki <h_borhan@hotmail.com>
2023-12-14 04:16:48 +0200

    tokenizing

---
+++ tokenization.c
@@ -0,0 +1,60 @@
+#include "simple_shell.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/**
+ * **tokenization - function that parse and tokenize the command
+ * @cmd: command to be parsed and tokinize
+ * Return: char
+ */
+char **tokenization(char *cmd)
+{
+	char *cmdcpy = NULL, *token = NULL;
+	const char *delim = " \t\n";
+	char **args = NULL;
+	int i = 1, j = 0;
+
+	if (cmd == NULL)
+	{
+		free(cmd);
+		return (NULL);
+	}
+	cmdcpy = _strdup(cmd);
+	token = strtok(cmd, delim);
+	while (token)
+	{
+		i++;
+		token = strtok(NULL, delim);
+	}
+	free(cmd);
+	cmd = NULL;
+	args = malloc(sizeof(char *) * (i + 1));
+	if (args == NULL)
+	{
+		free(cmdcpy);
+		free(args);
+		return (NULL);
+	}
+	token = strtok(cmdcpy, delim);
+	if (token == NULL)
+	{
+		free(cmdcpy);
+		free(args);
+		return (NULL);
+	}
+	while (token)
+	{
+		args[j] = _strdup(token);
+		token = strtok(NULL, delim);
+		j++;
+	}
+	free(cmdcpy);
+	cmdcpy = NULL;
+	args[j] = NULL;
+	return (args);
+}
