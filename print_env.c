85810ba1da3771236a9ee1b4a323ce98f3b083db
HebaZaki <h_borhan@hotmail.com>
2023-12-14 03:20:24 +0200

    env

--- print_env.c
+++ print_env.c
@@ -1,5 +1,9 @@
 #include "simple_shell.h"
 
+/**
+ * print_env - prints the current environment
+ */
+
 void print_env(void)
 {
 	int i = 0;
@@ -15,6 +19,7 @@ void print_env(void)
 
 		write(STDOUT_FILENO, environ[i], len);
 		write(STDOUT_FILENO, "\n", 1);
+
 		i++;
 	}
 }
